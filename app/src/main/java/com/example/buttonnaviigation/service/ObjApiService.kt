85c2a83e94cb82c9815af1e09fe050f98bc27c92
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2025-01-05 20:34:11 +0700

    Update ObjApiService.kt

--- app/src/main/java/com/example/buttonnaviigation/service/ObjApiService.kt
+++ app/src/main/java/com/example/buttonnaviigation/service/ObjApiService.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ObjApiService {
-    private const val BASE_URL = "http://192.168.1.8:8080"
+    private const val BASE_URL = "http://103.193.176.162:8000"
 
     val api: ApiService by lazy {
         Retrofit.Builder().baseUrl(BASE_URL)
